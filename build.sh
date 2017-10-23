8b9a6bfc2059bfed65da3340cbba6994f5d554ad
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2017-10-23 16:31:31 +0200

    Added bash dependency for nextflow

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 #Add here what kind of dependencies you need in general
 #Check for packages here: https://pkgs.alpinelinux.org/packages
-apk --update add wget curl build-base gcc git python-dev ca-certificates py-pip freetype-dev libpng libpng-dev
+apk --update add bash wget curl build-base gcc git python-dev ca-certificates py-pip freetype-dev libpng libpng-dev
 
 #Run the installation here or in the Singularity file itself, no matter which one 
 
