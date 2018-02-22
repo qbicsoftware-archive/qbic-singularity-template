9f92f57fd40b24087eb5139198221db7b83ab025
Alexander Peltzer <apeltzer@users.noreply.github.com>
2018-02-22 10:55:47 +0100

    Removed paths in template
    We don#t need them soon anymore !

--- build.sh
+++ build.sh
@@ -8,7 +8,3 @@ apt-get update
 apt-get install -y make automake gcc build-essential g++ cpp libc6-dev man-db autoconf pkg-config wget unzip python-pip python-dev
 
 #The actual tool installation should be done in the Singularity file itself to make the entire thing portable! 
-
-#Create mountpoints that we need
-mkdir -p /lustre_cfc/
-
